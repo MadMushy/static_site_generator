8d3af3f36f76bc6c45cdfaf7b512bba06925399f
da5734 <davidhels92@proton.me>
2025-12-03 01:55:04 -0800

    unit test

---
+++ src/htmlnode.py
@@ -0,0 +1,20 @@
+class HTMLNode:
+    def __init__(self, tag = None, value = None, children = None, props = None):
+        self.tag = tag
+        self.value = value
+        self.children = children
+        self.props = props
+
+    def to_html(self):
+        raise NotimplementedError("to_html method")
+
+    def props_to_html(self):
+        if not self.props:
+            return ""
+
+        return "".join(f'{key}="{value}"' for key,value in self.props.items())
+
+    def __repr__(self):
+        return f"HTMLTag = {self.tag!r}, Value = {self.value!r}, Children = {self.children!r}, Props = {self.props!r}"
+
+
