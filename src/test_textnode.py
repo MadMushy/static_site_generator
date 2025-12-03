0f122ce4fbde157bc93182aa7f88ff7a91346e5f
da5734 <davidhels92@proton.me>
2025-12-03 03:59:05 -0800

    unit test more

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,6 +1,7 @@
 import unittest
 
 from textnode import TextNode, TextType
+from htmlnode import HTMLNode
 
 
 class TestTextNode(unittest.TestCase):
@@ -8,6 +9,32 @@ class TestTextNode(unittest.TestCase):
         node = TextNode("This is a text node", TextType.BOLD)
         node2 = TextNode("This is a text node", TextType.BOLD)
         self.assertEqual(node, node2)
+
+    def test_props_to_html_multiple(self):
+        node = HTMLNode(
+            tag="a",
+            value="Click me",
+            props={"href": "https://google.com", "target": "_blank"},
+        )
+        # Compare actual output to expected value
+        self.assertEqual(
+            node.props_to_html(),
+            'href="https://google.com"target="_blank"'
+        )
+
+    def test_props_to_html_empty(self):
+        node = HTMLNode(tag="p", value="Hello", props={})
+        self.assertEqual(
+            node.props_to_html(),
+            ""
+        )
+
+    def test_repr(self):
+        node = HTMLNode(tag="p", value="Test", props={"class": "textbold"})
+        output = repr(node)
+        self.assertIn("HTMLTag = 'p'", output)
+        self.assertIn("Value = 'Test'", output)
+        self.assertIn("Props = {'class': 'textbold'}", output)
 
 
 if __name__ == "__main__":
